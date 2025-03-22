abfb89310df1cc60ae51514645cc23b79c969450
dkwach <dawid.kwach@gmail.com>
2025-03-22 22:17:19 +0100

    More flask app improvements

--- src/bq2589xpy/app/main.py
+++ src/bq2589xpy/app/main.py
@@ -3,13 +3,17 @@ import inspect
 import re
 from functools import lru_cache
 
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 
 from bq2589xpy.driver import Bq25895Driver, create
 from bq2589xpy.register import Register
 
 app = Flask(__name__)
 driver = create()
+
+
+# set FLASK_APP=bq2589xpy/app/main.py
+# python -m flask run
 
 
 @lru_cache
@@ -47,6 +51,31 @@ def registers():
             }
 
     return render_template("registers.html", registers=registers)
+
+
+@app.route("/update_register", methods=["POST"])
+def update_register():
+    reg_name = request.form["register"]
+    value = int(request.form["value"], 2)
+    reg = getattr(Bq25895Driver, reg_name)
+    reg_instance = reg()
+    reg_instance.value = value
+    driver.write(reg_instance)
+    return jsonify({"status": "success", "message": f"Register {reg_name} updated successfully."})
+
+
+@app.route("/update_bit_field", methods=["POST"])
+def update_bit_field():
+    reg_name = request.form["register"]
+    bit_field_name = request.form["bit_field"]
+    value = int(request.form["value"], 2)
+    reg = getattr(Bq25895Driver, reg_name)
+    reg_instance = driver.read(reg())
+    setattr(reg_instance, bit_field_name, value)
+    driver.write(reg_instance)
+    return jsonify(
+        {"status": "success", "message": f"Bit field {bit_field_name} in register {reg_name} updated successfully."}
+    )
 
 
 if __name__ == "__main__":
